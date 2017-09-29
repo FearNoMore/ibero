988259f000317c7c6d302af68cdf00ec1ea525f3
Rafael Moreno <spelljammer83@gmail.com>
2017-09-29 18:22:22 +0300

    added comment 2 main index.php

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ else {
         </div>
     </div>
 </div>
+<!-- Comment 4 git -->
 <script src="js/jquery-2.2.4.min.js"></script>
 <script src="js/slick.min.js"></script>
 <script src="js/script.js"></script>
